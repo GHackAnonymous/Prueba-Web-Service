f09f9c5f34c379fcf22a5e32c413c9cc8e796fca
ghackanonymous <ederaraia@gmail.com>
2015-06-08 17:27:46 +0200

    Intento de login3

--- src/java/Sesiones/Login.java
+++ src/java/Sesiones/Login.java
@@ -3,39 +3,33 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//package Sesiones;
+package Sesiones;
 
-//import BaseDatos.ConexionBD;
-//import BaseDatos.Querys;
-//import java.io.IOException;
-//import java.io.PrintWriter;
-//import java.sql.Date;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.sql.Statement;
-//import java.util.logging.Level;
-//import java.util.logging.Logger;
-//import javax.servlet.annotation.WebListener;
-//import javax.servlet.http.HttpSession;
-//import javax.servlet.http.HttpSessionEvent;
-//import javax.servlet.http.HttpSessionListener;
+import BaseDatos.ConexionBD;
+import BaseDatos.Querys;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
 
 /**
+ * Web application lifecycle listener.
  *
  * @author futpilari
  */
-
-//https://raibledesigns.com/rd/entry/java_web_application_security_part
-/*
-public class Sesion implements HttpSessionListener{
+public class Login implements HttpSessionListener {
 
     private Querys query = new Querys();
     private ConexionBD conexionBD = new ConexionBD(); 
     private boolean sesion = false;
     
-    @WebListener 
+    @Override
     public void sessionCreated(HttpSessionEvent se) {
-        
         Statement conexion = conexionBD.Conectar();
         String strSql = query.ConsultarUsuario("eder","qwerty");
         
@@ -53,14 +47,16 @@ public class Sesion implements HttpSessionListener{
             sesion = true;
         }
     }
+
+    @Override
     public void sessionDestroyed(HttpSessionEvent se) {
         HttpSession session = se.getSession();
         System.out.println("A las " + getTime() + " se destruyo la sesion con ID: "
         + session.getId());
         sesion = false;
     }
+    
     private String getTime() {
         return new Date(System.currentTimeMillis()).toString();
     } 
 }
-*/
