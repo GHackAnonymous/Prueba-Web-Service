672856b363db65bacecf352087a309182db762c6
ghackanonymous <ederaraia@gmail.com>
2015-06-08 16:38:58 +0200

    Intento de login2

--- src/java/Sesiones/Sesion.java
+++ src/java/Sesiones/Sesion.java
@@ -15,10 +15,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebListener;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
@@ -27,91 +24,20 @@ import javax.servlet.http.HttpSessionListener;
  *
  * @author futpilari
  */
-public class Sesion extends HttpServlet implements HttpSessionListener{
+
+//https://raibledesigns.com/rd/entry/java_web_application_security_part
+
+public class Sesion implements HttpSessionListener{
 
     private Querys query = new Querys();
-    private ConexionBD conexionBD = new ConexionBD();
-    
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    
-    private HttpServletRequest request; 
+    private ConexionBD conexionBD = new ConexionBD(); 
     private boolean sesion = false;
     
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            
-            this.request = request;
-            
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Sesion</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            if(sesion == true){
-                out.println("Sesion iniciada");
-            }else{
-                out.println("Sesion fallida");
-            }
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+    @WebListener 
     public void sessionCreated(HttpSessionEvent se) {
         
         Statement conexion = conexionBD.Conectar();
-        String strSql = query.ConsultarUsuario(this.request.getParameter("nombre"), this.request.getParameter("password"));
+        String strSql = query.ConsultarUsuario("eder","qwerty");
         
         ResultSet result = null;
         try {
